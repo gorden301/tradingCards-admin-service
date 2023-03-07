b3d3447e1d48520b4632e44be66926e7ef4b0bbc
gorden301 <1019108859@qq.com>
2023-03-07 18:21:35 +0800

    feat: 增加服务端获取订单列表分页

--- controller/order.js
+++ controller/order.js
@@ -9,10 +9,36 @@ router.get('/orderList', async (ctx, next) => {
     const getListRes = await callCloudFn(ctx, 'order', {
         $url: 'getOrderList'
     })
-    console.log(getListRes)
+    const orderListCountQuery = `db.collection('orderList').count()`
+    const res = await callCloudDB(ctx, 'databasecount', orderListCountQuery)
+    const pageDataQuery = `db.collection('orderList').orderBy('createTime', 'desc').skip(10).limit(10).get()`
+    const pageRes = await callCloudDB(ctx, 'databasequery', pageDataQuery)
+    console.log('pageRes', pageRes)
+    // console.log(getListRes)
     ctx.body = {
         code: 0,
         data: JSON.parse(getListRes?.resp_data).data,
+        msg: '请求成功'
+    }
+})
+
+router.post('/newOrderList', async (ctx, next) => {
+    const params = ctx.request.body
+    const orderListCountQuery = `db.collection('orderList').count()`
+    const res = await callCloudDB(ctx, 'databasecount', orderListCountQuery)
+    const pageDataQuery = `db.collection('orderList').orderBy('createTime', 'desc').skip(${(params.page - 1) * 10}).limit(10).get()`
+    const pageRes = await callCloudDB(ctx, 'databasequery', pageDataQuery)
+    const dataArr = pageRes?.data.map((item) => {
+        return JSON.parse(item)
+    })
+    console.log('pageRes', pageRes)
+    // console.log(getListRes)
+    ctx.body = {
+        code: 0,
+        data: {
+            total: res.count,
+            data: dataArr
+        },
         msg: '请求成功'
     }
 })
